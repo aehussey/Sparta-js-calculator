fb8eccbe622414f7a57a08d7837f11f05b09eee0
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2019-04-08 16:45:19 +0100

    Finished basic calculator

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
-// Basic Calculator
+// Prompt "Do you want advanced or basic"
+//Prompt "What Numbers please?"
+// Alert Answer
 
-// Advanced Calculator
+var calculatorType = prompt("What Kind of calculator would you like to use, (a)dvanced or (b)asic?")
+
+if (calculatorType.toLowerCase() == "a" || calculatorType.toLowerCase() == "advanced") {
+  // Advanced Calculator
+
+}
+
+else if (calculatorType.toLowerCase() == "b" || calculatorType.toLowerCase() == "basic") {
+  // Basic Calculator
+  var equation = prompt("Please input your equation");
+  alert(eval(equation));
+
+}
+// parseInt() - change string to int
+
 
 // BMI calculator
