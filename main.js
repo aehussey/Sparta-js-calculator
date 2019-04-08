90bbdff4a1a888b56f965c6f746409cdc6f72fbf
Tech-A06 <tech-a06@tech-a06s-air.lan>
2019-04-08 18:51:52 +0100

    Added Trip calculator and cleaned the code up

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
-// Prompt "Do you want advanced or basic"
-//Prompt "What Numbers please?"
-// Alert Answer
-
-var calculatorType = prompt("What Kind of calculator would you like to use, (a)dvanced, (b)asic? or the BMI Calculator")
+var calculatorType = prompt("What Kind of calculator would you like to use, (a)dvanced, (b)asic?, the BMI Calculator or the Trip Calculator")
 
 if (calculatorType.toLowerCase() == "a" || calculatorType.toLowerCase() == "advanced") {
-  var calculationType = prompt("Would you like to use the 'power' or 'square root' feature");
+
+// Advanced Calculator
+
+  var calculationType = prompt("Would you like to use the 'power' or 'square root' feature?");
 
     if (calculationType.toLowerCase() == "power") {
-      var raise = prompt("What number would you like to have raised");
+      var raise = prompt("What number would you like to have raised?");
       var power = prompt("And what power would you like that to be raised to?");
-      alert(parseInt(raise) ** parseInt(power));
+      alert("The answer is " + (parseInt(raise) ** parseInt(power)));
     }
     else {
       var sqrt = prompt("What number would you like to have square rooted?");
 
-      alert(Math.sqrt(parseInt(sqrt)));
+      alert("The answer is " + Math.sqrt(parseInt(sqrt)));
     }
 
 }
 
 else if (calculatorType.toLowerCase() == "b" || calculatorType.toLowerCase() == "basic") {
+
   // Basic Calculator
+
   var equation = prompt("Please input your equation. (Use * for multiplication)");
-  alert(eval(equation));
+  alert("The answer is " + eval(equation));
 
 }
-// parseInt() - change string to int
 
 
-// BMI calculator
+// BMI Calculator
 
 else if (calculatorType.toLowerCase() == "bmi" || calculatorType.toLowerCase() == "bmi calculator") {
   var measurementSystem = prompt("Would you like to use the Metric or Imperial system")
@@ -44,4 +44,37 @@ else if (calculatorType.toLowerCase() == "bmi" || calculatorType.toLowerCase() =
     var weightKilos = prompt("What is your weight in Kilos?");
     var heightMeters = prompt("What is your height in Meters?");
     alert("Your BMI is " + (parseInt(weightKilos)/(parseInt(heightMeters)*parseInt(heightMeters))));}
+}
+
+// TRIP CALCULATOR
+
+else {
+  var distance = prompt("How far are you going? (In Miles)");
+  var fuelEfficiency = prompt("What is the fuel efficiency of your car? (In MPG)");
+  var costPerGallon = prompt("What is your cost per gallon?");
+  var speed = prompt("What will your average speed be? (In MPH)");
+  var time = parseInt(distance)/parseInt(speed)
+
+  if (speed <= 60) {
+    var cost = (parseInt(distance) / parseInt(fuelEfficiency)) * parseInt(costPerGallon);
+    alert("The journey will take you " + time.toFixed(2) + " hours and cost you £" + cost + ".");
+  }
+
+  else if (speed > 60) {
+
+    fuelEfficiency = fuelEfficiency - 2*(speed-60);
+
+    if (fuelEfficiency >= 0) {
+
+      var cost = (parseInt(distance) / parseInt(fuelEfficiency)) * parseInt(costPerGallon);
+      alert("The journey will take you " + time.toFixed(2) + " hours and cost you £" + cost + ".");
+
+  }
+
+    else {
+      alert("This is invalid")
+    }
+
+  }
+
 }
